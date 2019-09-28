deabd53a8314826a307e7e91dfe09abfbd93a4b8
Alex Komissarov <k.a.komissar@gmail.com>
2019-09-28 20:45:44 +0500

    Tests for parse_literal

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,13 @@ fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), 
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[test]
+fn literal_parser() {
+    let parse_joe = match_literal("Hello Joe!");
+    assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
+    assert_eq!(
+        Ok((" Hello Robert!", ())),
+        parse_joe("Hello Joe! Hello Robert!")
+    );
+    assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
 }
