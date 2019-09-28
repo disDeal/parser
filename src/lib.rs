62e5aea25c1750bff38253481c3d6c5a86e52964
Alex Komissarov <k.a.komissar@gmail.com>
2019-09-28 20:28:54 +0500

    Init

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+struct Element {
+    name: String,
+    attribute: Vec<(String, String)>,
+    children: Vec<Element>,
+}
+
+fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
+    match input.chars().next() {
+        Some('a') => Ok((&input['a'.len_utf8()..], ())),
+        _ => Err(input),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
