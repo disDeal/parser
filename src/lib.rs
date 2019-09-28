453b2bee383452a7ffd5fbbdfc4752180aaeab29
Alex Komissarov <k.a.komissar@gmail.com>
2019-09-28 20:39:04 +0500

    Added pattern constructor

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,13 @@ fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
     }
 }
 
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
+        _ => Err(input),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
